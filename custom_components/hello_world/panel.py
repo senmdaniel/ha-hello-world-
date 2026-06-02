207ed1cc96b47396946f8525a379cdb7eebc0af7
senmdaniel <senmdaniel@hotmail.com>
2026-06-02 10:39:50 +0200

    Update panel.py

--- custom_components/hello_world/panel.py
+++ custom_components/hello_world/panel.py
@@ -8,6 +8,6 @@ async def async_setup_panel(hass):
         sidebar_icon="mdi:web",
         frontend_url_path="hello-world",
         config={
-            "url": "https://example.org"
+            "url": "https://senmdaniel.github.io/ha-hello-world-/"
         },
     )
